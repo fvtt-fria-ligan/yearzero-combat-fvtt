d077a3be96d002e29466b8c603ff9cba79fc6aae
Stefouch <47865544+Stefouch@users.noreply.github.com>
2022-06-22 23:40:06 +0200

    chore: 🔧 Activate all custom documentClass

--- src/yze-combat.js
+++ src/yze-combat.js
@@ -17,20 +17,14 @@ import { YZEC } from '@module/config';
 // import { registerSheets } from '@system/sheets';
 import { initializeHandlebars } from '@module/handlebars';
 import { registerSystemSettings } from '@module/settings';
+import YearZeroCards from './combat/cards';
 import YearZeroCombat from './combat/combat';
 import YearZeroCombatant from './combat/combatant';
+import YearZeroCombatTracker from './sidebar/combat-tracker';
 
 /* ------------------------------------------ */
 /*  Foundry VTT Initialization                */
 /* ------------------------------------------ */
-
-/**
- * Debugging
- */
-/** @__PURE__ */ (async () => {
-  CONFIG.debug.hooks = true;
-  console.warn('HOOKS DEBUG ACTIVATED: ', CONFIG.debug.hooks);
-})();
 
 Hooks.once('init', () => {
   logger.log('YZEC | Initializing the Year Zero Combat Module');
@@ -40,9 +34,17 @@ Hooks.once('init', () => {
 
   // Records configuration values.
   CONFIG.YZE_COMBAT = YZEC;
-  // TODO Combat
-  // CONFIG.Combat.documentClass = YearZeroCombat;
-  // CONFIG.Combatant.documentClass = YearZeroCombatant;
+  CONFIG.Combat.documentClass = YearZeroCombat;
+  CONFIG.Combatant.documentClass = YearZeroCombatant;
+  CONFIG.Cards.documentClass = YearZeroCards;
+  CONFIG.Cards.presets = {
+    initiative: {
+      label: 'YZEC.InitiativeDeckPreset',
+      src: 'systems/yze-combat/cards/initiative-deck.json',
+      type: 'deck',
+    },
+  };
+  CONFIG.ui.combat = YearZeroCombatTracker;
 
   // registerSheets();
   initializeHandlebars();
