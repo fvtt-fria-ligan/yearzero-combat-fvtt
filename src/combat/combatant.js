33145acc1525d29e0fcafaf92bafdd74e94eb47a
DrOgres <drogres@gmail.com>
2022-06-21 21:31:25 -0500

    feat: ✨ initial combatant getters and setters

--- src/combat/combatant.js
+++ src/combat/combatant.js
@@ -1,3 +1,37 @@
 export default class YearZeroCombatant extends Combatant {
   // TODO https://gitlab.com/peginc/swade/-/blob/develop/src/module/documents/SwadeCombatant.ts
+
+  async setCardValue(cardValue) {
+    return this.setFlag('yzec', 'cardValue', cardValue);
+  }
+
+  get cardValue() {
+    return this.getFlag('yzce', 'cardValue');
+  }
+
+  get cardString() {
+    return this.getFlag('yzce', 'cardString');
+  }
+
+  async setCardString(cardString) {
+    return this.setFlag('yzce', 'cardString', cardString);
+  }
+  /**
+   * 
+   * @param {Combatant} target    the combatant with which this combatant will swap a card
+   */
+  // TODO  recalculate the turn order on complettion
+  async swapInitiativeCard(target) {
+    const combatantCard = this.cardValue();
+    this.setCardValue(target.cardValue());
+    target.setCardValue(combatantCard);
+  }
+
+  /**
+   * @override
+   * 
+   *
+   **/
+
+  rollInitiative() {}
 }
