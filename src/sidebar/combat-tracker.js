0fa53fff9ffd29f6629af7eb52e569b335b1db1c
aMediocreDad <amediocredad@outlook.com>
2022-06-26 13:44:10 +0200

    feat: ✨ Handle DOM updates when toggling buttons

--- src/sidebar/combat-tracker.js
+++ src/sidebar/combat-tracker.js
@@ -17,9 +17,11 @@ export default class YearZeroCombatTracker extends CombatTracker {
     const combat = this.viewed;
     const combatant = combat.combatants.get(li.dataset.combatantId);
     const eventName = btn.dataset.event;
+    const property = btn.dataset.property;
     const eventData = {
       combat,
       combatant,
+      property,
       event: eventName,
       origin: btn,
     };
@@ -29,14 +31,20 @@ export default class YearZeroCombatTracker extends CombatTracker {
         options,
       });
     Hooks.call(`${MODULE_NAME}.${eventName}`, eventData);
+    combatant.setProperty(property, !combatant.getProperty(property));
   }
 
   /** @override */
   async getData(options) {
     const data = await super.getData(options);
+    const turns = data.turns.map(turn => ({
+      ...turn,
+      ...this.#setTurnProperties(data, turn),
+    }));
     const buttons = await this.#getButtonConfig();
     return {
       ...data,
+      turns,
       buttons,
     };
   }
@@ -45,6 +53,29 @@ export default class YearZeroCombatTracker extends CombatTracker {
   async #getButtonConfig() {
     const { src } = CONFIG.YZE_COMBAT.CombatTracker.config;
     const { buttons } = await foundry.utils.fetchJsonWithTimeout(src);
-    return buttons;
+    const sortedButtons = buttons.reduce((acc, button) => {
+      const { visibility, ...buttonConfig } = button;
+      if (visibility === 'gm') {
+        acc.gmButtons.push(buttonConfig);
+      }
+      else if (visibility === 'owner') {
+        acc.ownerButtons.push(buttonConfig);
+      }
+      else {
+        acc.commonButtons.push(buttonConfig);
+      }
+      return acc;
+    }, {
+      commonButtons: [],
+      gmButtons: [],
+      ownerButtons: [],
+    });
+    return sortedButtons;
+  }
+
+  #setTurnProperties(data, turn) {
+    const { id } = turn;
+    const combatant = data.combat.combatants.get(id);
+    return combatant.data.flags['yze-combat'] ?? {};
   }
 }
