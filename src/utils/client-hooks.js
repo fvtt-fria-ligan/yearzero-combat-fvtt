48bea18667c1aa4c099f25d903149ca57c089f40
Cussa Mitre <cussa.mitre@gmail.com>
2025-08-24 11:03:40 +0100

    fix: 🐛 Client Hooks control module configuration
    - Add new setting: `configured`
    - When calling the `register`, check if the module is already `configured`.
    - If `once` is `true` and `configured` is true, return
    - Add new parameter `finishConfiguration`
    - `setInitiaveDeck` and `setDiscardPile` do not mark the module as `configured`
    - Improve documentation

--- src/utils/client-hooks.js
+++ src/utils/client-hooks.js
@@ -1,4 +1,4 @@
-import { CARD_STACK, MODULE_ID } from '@module/constants';
+import { CARD_STACK, MODULE_ID, SETTINGS_KEYS } from '@module/constants';
 
 /**
  * An abstract class (cannot be instantiated) with utility methods
@@ -14,40 +14,56 @@ export default class YearZeroCombatHook {
    * Registers game settings for this module.
    * @param {Object.<string, boolean|string|number>} settings
    *   Pairs of key-value to register in the game settings
-   * @param {boolean} [once=true] Whether to register the setting only if the setting is undefined
+   * @param {boolean} [once=true] Only register the settings if the registration never happened
+   * @param {boolean} [finishConfiguration=true] Whether to set the module as configured after registration
    * @returns {Promise.<void>}
    */
-  static async register(settings, once = true) {
-    if (!game.user.isGM) return;
+  static async register(settings, once = true, finishConfiguration = true) {
+    if (!game.user?.isGM) return;
+
+    const isConfigured = await game.settings.get(MODULE_ID, SETTINGS_KEYS.CONFIGURED);
+    if (isConfigured && once) return;
+
     for (const [k, v] of Object.entries(settings)) {
       try {
-        if (once && game.settings.get(MODULE_ID, k)) continue;
         await game.settings.set(MODULE_ID, k, v);
       }
       catch (err) {
         console.error(err);
       }
     }
+
+    if (finishConfiguration) { await this.finishConfiguration(); }
+  }
+
+  static async finishConfiguration() {
+    if (!game.user?.isGM) return;
+
+    await game.settings.set(MODULE_ID, SETTINGS_KEYS.CONFIGURED, true);
   }
 
   /**
    * Sets the initiative deck's ID (or name) in the game settings.
+   * Should be called before the `register` method, or called with the `once` parameter set to `false`.
    * @param {string}   id         ID or name of the card stack to register
    * @param {boolean} [once=true] Whether to register the setting only if the setting is undefined
+   * @param {boolean} [finishConfiguration=false] Whether to set the module as configured after registration
    * @returns {Promise.<void>}
    */
-  static async setInitiativeDeck(id, once = true) {
-    return this.register({ [CARD_STACK.INITIATIVE_DECK]: id }, once);
+  static async setInitiativeDeck(id, once = true, finishConfiguration = false) {
+    return this.register({ [CARD_STACK.INITIATIVE_DECK]: id }, once, finishConfiguration);
   }
 
   /**
    * Sets the discard pile's ID (or name) in the game settings.
+   * Should be called before the `register` method, or called with the `once` parameter set to `false`.
    * @param {string}   id         ID or name of the card stack to register
    * @param {boolean} [once=true] Whether to register the setting only if the setting is undefined
+   * @param {boolean} [finishConfiguration=false] Whether to set the module as configured after registration
    * @returns {Promise.<void>}
    */
-  static async setDiscardPile(id, once = true) {
-    return this.register({ [CARD_STACK.DISCARD_PILE]: id }, once);
+  static async setDiscardPile(id, once = true, finishConfiguration = false) {
+    return this.register({ [CARD_STACK.DISCARD_PILE]: id }, once, finishConfiguration);
   }
 
   /**
