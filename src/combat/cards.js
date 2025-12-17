6edb7aa71cb20f344360a11a115a0b143d052e9c
Pafvel <91607056+Pafvel@users.noreply.github.com>
2025-12-17 22:25:28 +0100

    fix: 🐛 Initiative cards not suffled in case of drawsize >1 (Fordidden Lands) #49

--- src/combat/cards.js
+++ src/combat/cards.js
@@ -15,11 +15,12 @@ export default class YearZeroCards extends Cards {
    * Draws cards for initiative.
    * @param {Cards}   to          The cards document to which the cards are deposited
    * @param {number} [qty=1]      How many cards to draw
-   * @param {number} [drawMode=0] How to draw the cards, e.g. from the top of the deck
+   * @param {number} [drawMode=2] How to draw the cards, e.g. from the top of the deck
+   * @param {Card[]} [cards]      The cards to draw (if they have been peeked at for example)
    * @returns {Promise.<Card[]>} An array of drawn cards, in the order they were drawn
    * @see {@link CONST.CARD_DRAW_MODES}
    */
-  async drawInitiative(to, qty = 1, drawMode = foundry.CONST.CARD_DRAW_MODES.TOP) {
+  async drawInitiative(to, qty = 1, drawMode = foundry.CONST.CARD_DRAW_MODES.RANDOM, cards = null) {
     // Exits early if invalid deck type.
     if (this.type !== 'deck') {
       const msg = game.i18n.localize('YZEC.WARNING.InvalidDeckType');
@@ -27,7 +28,7 @@ export default class YearZeroCards extends Cards {
     }
 
     // Draws the cards.
-    const drawn = this._drawCards(qty, drawMode);
+    const drawn = cards ?? this._drawCards(qty, drawMode);
 
     // Processes the card data.
     const toCreate = [];
@@ -47,6 +48,58 @@ export default class YearZeroCards extends Cards {
       this.deleteEmbeddedDocuments('Card', toDelete),
     ]);
 
+    // Returns the drawn cards.
+    return this.updateEmbeddedDocuments('Card', toUpdate);
+  }
+
+  /**
+   * Peek at cards for initiative. Sets them as drawn but does not remove them from the deck.
+   * @param {number} [qty=1]      How many cards to draw
+   * @param {number} [drawMode=2] How to draw the cards, e.g. from the top of the deck
+   * @returns {Card[]} An array of cards, in the order they were drawn
+   * @see {@link CONST.CARD_DRAW_MODES}
+   */
+  async peekInitiative(qty = 1, drawMode = foundry.CONST.CARD_DRAW_MODES.RANDOM) {
+    // Exits early if invalid deck type.
+    if (this.type !== 'deck') {
+      const msg = game.i18n.localize('YZEC.WARNING.InvalidDeckType');
+      throw new TypeError(msg);
+    }
+
+    // Draws the cards.
+    const drawn = this._drawCards(qty, drawMode);
+
+    // Processes the card data.
+    const toUpdate = [];
+    for (const card of drawn) {
+      if (card.isHome) {
+        // Set to drawn so that other players can't draw it.
+        toUpdate.push({ _id: card.id, drawn: true });
+      }
+    }
+    // Returns the drawn cards.
+    return this.updateEmbeddedDocuments('Card', toUpdate);
+  }
+
+  /**
+   * Returns peeked at cards. Sets them as not drawn.
+   * @param {Card[]} [cards]      The cards to return
+   * @returns {Card[]}            An array of cards, in the order they were drawn
+   */
+  async returnPeekedAtCards(cards) {
+    // Exits early if invalid deck type.
+    if (this.type !== 'deck') {
+      const msg = game.i18n.localize('YZEC.WARNING.InvalidDeckType');
+      throw new TypeError(msg);
+    }
+
+    // Processes the card data.
+    const toUpdate = [];
+    for (const card of cards) {
+      if (card.isHome) {
+        toUpdate.push({ _id: card.id, drawn: false });
+      }
+    }
     // Returns the drawn cards.
     return this.updateEmbeddedDocuments('Card', toUpdate);
   }
